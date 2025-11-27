fd4dca194e431bafaf2d5f622bb0af42b0df0d8a
Amelina123 <146894964+Amelina123@users.noreply.github.com>
2025-11-27 11:42:03 +0000

    Add simple_interface.py

---
+++ src/simple_interface.py
@@ -0,0 +1,40 @@
+import re
+from emotion_classifier import EmotionClassifier
+from config import MODEL_PATH
+
+def clean_text(raw_text: str) -> str:
+    raw_text = raw_text.lower()
+    raw_text = raw_text.strip()
+    raw_text = re.sub(r"[^\w\s.,!?']", " ", raw_text)
+    raw_text = re.sub(r"#(\w+)", r"\1", raw_text)  
+    raw_text = re.sub(r"\s+", " ", raw_text)
+    return raw_text
+
+def main():
+    print(" Hello, I am Emotion classifier - simple interface.")
+    print("Type :q or exit to leave.\n")
+    print("Type a sentence to classify.\n")
+
+    classifier = EmotionClassifier(MODEL_PATH)
+
+    exit_conditions = ("q", "exit")
+    while True:
+        user_text = input("Enter a sentence: ").strip()
+        
+        if user_text in exit_conditions:
+            print("Goodbye!")
+            break
+        
+        if not user_text:
+            print("Please enter non-empty text.\n")
+            continue
+        
+        cleaned_text = clean_text(user_text)
+        prediction = classifier.classify(cleaned_text)
+
+        print(f"Result: {prediction}\n")
+
+
+
+if __name__ == "__main__":
+    main()
